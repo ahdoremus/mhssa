a294cb4646f533b856ab6bb8aad474a18314fd59
Tony Erskine <tony@sharptop.co>
2018-10-31 13:38:47 -0400

    display all standings complete

--- standings/standings-component.js
+++ standings/standings-component.js
@@ -1,9 +1,5 @@
 const API_URL = 'https://airtable.sharptop.io'
 var games = []
-const gender = {boys: "Boys", girls: "Girls"}
-const skillLevel = {var: "Varsity", jv: "Junior Varsity", jh: "Junior High"}
-var activeGender = "Boys"
-var activeSkillLevel = "Varsity"
 
 $(document).ready(function () {
     displayGames()
@@ -49,11 +45,44 @@ async function displayGames() {
     games.forEach(game => {
         addGameToStandings(game, game.competitive ? competitiveStandings : noncompetitiveStandings);
     })
-    console.log(`Competitive`, competitiveStandings)
-    console.log(`Noncompetitive`, noncompetitiveStandings)
-    // for (var team in competitiveStandings) {
-    //     console.log(team, competitiveStandings[team])
-    // }
+    // console.log(`Competitive`, competitiveStandings)
+    // console.log(`Noncompetitive`, noncompetitiveStandings)
+    for (var gender in competitiveStandings) {
+        console.log(gender, competitiveStandings[gender])
+        // $('#st-standings-table').append(`<div class="row"><div class="col-11 offset-1 st-align-left"><strong>${gender}</strong></div></div>`)
+        for (var skillLevel in competitiveStandings[gender]) {
+            $('#st-standings-table').append(`
+                <div class="row">
+                    <div class="col-12"><strong>${skillLevel} - ${gender}</strong></div>
+                </div>
+            `)
+            console.log(skillLevel, competitiveStandings[gender][skillLevel])
+            let list = []
+            for (var team in competitiveStandings[gender][skillLevel]) {
+                list.push({
+                    name: team,
+                    won: competitiveStandings[gender][skillLevel][team].won + noncompetitiveStandings[gender][skillLevel][team].won,
+                    lost: competitiveStandings[gender][skillLevel][team].lost + noncompetitiveStandings[gender][skillLevel][team].lost,
+                    tied: competitiveStandings[gender][skillLevel][team].tied + noncompetitiveStandings[gender][skillLevel][team].tied,
+                })
+            }
+
+            list.sort(compareStandings)
+            let i = 1
+            list.forEach((team, i) => {
+                console.log(team)
+                $('#st-standings-table').append(`
+                    <div class="row">
+                        <div class="col-2">${i + 1}</div>
+                        <div class="col-4 team-column">${team.name}</div>
+                        <div class="col-2">${team.won}</div>
+                        <div class="col-2">${team.lost}</div>
+                        <div class="col-2">${team.tied}</div>
+                    </div>
+                `)
+            })
+        }
+    }
 }
 
 function fetch(objectType) {
@@ -62,10 +91,26 @@ function fetch(objectType) {
         url: API_URL,
         dataType: 'json',
         data: JSON.stringify({
-            "baseId": "appNtmri6lpYTOimo",
-            "tableName": objectType,
-            "action": "select"
+            'baseId': 'appNtmri6lpYTOimo',
+            'tableName': objectType,
+            'action': 'select'
         })
     })
 
+}
+
+function compareStandings(a, b) {
+    if (a.won > b.won)
+        return -1;
+    if (a.won < b.won)
+        return 1;
+    if (a.lost < b.lost)
+        return -1;
+    if (a.lost > b.lost)
+        return 1;
+    if (a.tied < b.tied)
+        return -1;
+    if (a.tied > b.tied)
+        return 1;
+    return 0;
 }
